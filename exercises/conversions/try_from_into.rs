b7a422d9872f2b4d5c59b8eaa92e5cba60e06931
bnlrnz <lorenzb@tu-freiberg.de>
2021-01-07 13:09:09 +0100

    all done

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -11,8 +11,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -25,19 +23,56 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if (0..=255).contains(&tuple.0) == false {
+            return Err(String::from("R must be between 0 to 255"))
+        }
+        if (0..=255).contains(&tuple.1) == false {
+            return Err(String::from("G must be between 0 to 255"))
+        }
+        if (0..=255).contains(&tuple.2) == false {
+            return Err(String::from("B must be between 0 to 255"))
+        }
+        Ok(Color{red: tuple.0 as u8, green: tuple.1 as u8, blue: tuple.2 as u8})
+    }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if (0..=255).contains(&arr[0]) == false {
+            return Err(String::from("R must be between 0 to 255"))
+        }
+        if (0..=255).contains(&arr[1]) == false {
+            return Err(String::from("G must be between 0 to 255"))
+        }
+        if (0..=255).contains(&arr[2]) == false {
+            return Err(String::from("B must be between 0 to 255"))
+        }
+        Ok(Color{red: arr[0] as u8, green: arr[1] as u8, blue: arr[2] as u8})
+    }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = String;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(String::from("There should be 3 components, R, G and B."))
+        }
+
+        if (0..=255).contains(&slice[0]) == false {
+            return Err(String::from("R must be between 0 to 255"))
+        }
+        if (0..=255).contains(&slice[1]) == false {
+            return Err(String::from("G must be between 0 to 255"))
+        }
+        if (0..=255).contains(&slice[2]) == false {
+            return Err(String::from("B must be between 0 to 255"))
+        }
+        Ok(Color{red: slice[0] as u8, green: slice[1] as u8, blue: slice[2] as u8})
+    }
 }
 
 fn main() {
